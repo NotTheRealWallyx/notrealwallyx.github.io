c2b2fdb7b45c72cb42f6029b362add6f3edb3003
Mikel Sanchez <mikelsanchezmart@gmail.com>
2024-11-16 17:09:21 +0000

    basic home screen with some animations, no mobile support

---
+++ src/scripts/main.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedText = document.querySelector('.animated-text');
+    const texts = ['Head of Engineering and Developer.', 'Based in London.'];
+
+    let currentIndex = 0;
+    let isDeleting = false;
+    let currentText = '';
+    let charIndex = 0;
+    let delay = 2000;
+
+    function type() {
+        const fullText = texts[currentIndex];
+
+        if (isDeleting) {
+            charIndex--;
+            currentText = fullText.substring(0, charIndex);
+        } else {
+            charIndex++;
+            currentText = fullText.substring(0, charIndex);
+        }
+
+        animatedText.textContent = currentText;
+
+        if (!isDeleting && charIndex === fullText.length) {
+            setTimeout(() => {
+                isDeleting = true;
+                type();
+            }, delay);
+        } else if (isDeleting && charIndex === 0) {
+            isDeleting = false;
+            currentIndex = (currentIndex + 1) % texts.length;
+            setTimeout(type, 500);
+        } else {
+            setTimeout(type, isDeleting ? 50 : 100);
+        }
+    }
+
+
+    function navigationEvents() {
+        const navLinks = document.querySelectorAll('.navigation a');
+
+        navLinks.forEach(link => {
+            link.addEventListener('mouseover', () => {
+                link.style.color = '#ff9800';
+            });
+            link.addEventListener('mouseout', () => {
+                link.style.color = '#ffffff';
+            });
+        });
+    }
+    type();
+    navigationEvents();
+});
