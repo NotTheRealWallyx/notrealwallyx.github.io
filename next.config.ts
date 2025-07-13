39a463a43fad4447ec43abeae040c31cb3a08db4
Mikel Sanchez <mikelsanchezmart@gmail.com>
2025-07-13 17:12:22 +0200

    remove asset and base paths on the next config file

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
-const repoName = 'notrealwallyx.github.io';
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig: import('next').NextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
 };
 
 export default nextConfig;
